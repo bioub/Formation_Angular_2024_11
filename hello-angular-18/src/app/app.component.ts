bffaad4c9caad4e31551ae370d39666c90c17d2e
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-11-07 11:26:46 +0100

    if/for

--- hello-angular-18/src/app/app.component.ts
+++ hello-angular-18/src/app/app.component.ts
@@ -5,11 +5,14 @@ import { ClockComponent } from "./clock/clock.component";
 import { MyFormComponent } from "./my-form/my-form.component";
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { MultiStateButtonComponent } from './multi-state-button/multi-state-button.component';
+import { SelectComponent } from "./select/select.component";
+import { MyButtonComponent } from "./my-button/my-button.component";
+import { MyCardComponent } from "./my-card/my-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloComponent, ClockComponent, MyFormComponent, HelloWorldComponent, MultiStateButtonComponent],
+  imports: [RouterOutlet, HelloComponent, ClockComponent, MyFormComponent, HelloWorldComponent, MultiStateButtonComponent, SelectComponent, MyButtonComponent, MyCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ hello-angular-18/src/app/select/select.component.ts
@@ -0,0 +1,17 @@
+import { NgClass, NgIf } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  imports: [NgClass, NgIf],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+  // encapsulation: ViewEncapsulation.ShadowDom
+})
+export class SelectComponent {
+  items = ['Item 1', 'Item 2', 'Item 3'];
+  value = 'Item 2';
+  isMenuOpen = false;
+
+}
