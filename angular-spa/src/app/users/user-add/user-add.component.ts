efd4b24fac6c4e4d03ab00144a691feb004284ec
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-11-08 14:39:08 +0100

    Forms

--- angular-spa/src/app/users/user-add/user-add.component.ts
+++ angular-spa/src/app/users/user-add/user-add.component.ts
@@ -1,28 +1,45 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../user.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-add',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './user-add.component.html',
   styleUrl: './user-add.component.scss'
 })
-export class UserAddComponent {
+export class UserAddComponent implements OnInit {
+
 
   private userService = inject(UserService);
 
-  onSubmit(event: SubmitEvent) {
-    event.preventDefault();
+  userForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('romain.bohdanowicz@formation.tech', [Validators.email]),
+    phone: new FormControl('06 00 00 00 00', []),
+  });
+
+  onSubmit() {
+    if (this.userForm.invalid) {
+      return;
+    }
 
     // TODO
-    // Envoyer une requete POST vers https//jsonplaceholder.typicode.com/users
-    // Si ça se passe bien
-    // Ajouter le user créé au menu de gauche
+    // Envoyer la requete POST
+    const user = this.userForm.value as User;
 
     this.userService.userAdded$.next({
       id: 11,
-      name: 'Romain',
+      ...user,
+    });
+  }
+
+  ngOnInit(): void {
+    this.userForm.valueChanges.subscribe((user) => {
+      console.log(user);
     });
   }
 }
--- hello-angular-16/src/app/app.module.ts
+++ hello-angular-16/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { MyFormComponent } from './my-form/my-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    MyFormComponent
   ],
   imports: [
     BrowserModule, // qui lui même donne accès à CommonModule (UpperCasePipe, DatePipe, NgIfDirective...)
-    AppRoutingModule
+    AppRoutingModule,
+    // FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
